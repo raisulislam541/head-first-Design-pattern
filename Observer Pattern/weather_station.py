cee96cec511e5664f8ff1c47964e96d9c173ce5b
Raisul <raisulislam541@gmail.com>
2022-08-01 12:37:43 +0600

    add: abstract methods for  subjects

--- Observer Pattern/weather_station.py
+++ Observer Pattern/weather_station.py
@@ -2,9 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class Subject(ABC):
+
+    @abstractmethod
     def register_observer(self):
         raise "not implemented"
 
+    @abstractmethod
     def remove_observer(self):
+        raise "not implemented"
+
+    @abstractmethod
+    def notify_observers(self):
+        raise "not implemented"
 
 
